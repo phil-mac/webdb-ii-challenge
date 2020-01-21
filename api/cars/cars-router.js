327d70de315f32ffb4b627cdd97f5f21cc9bba8a
Phillip MacEachron <phillipmaceachron@Phillips-MacBook-Pro.local>
2020-01-21 15:12:47 -0800

    added put and delete to router

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -4,19 +4,19 @@ const db = require('../../data/dbConfig');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get('/', (req, res, next) => {
     db('cars')
     .then(cars => {
         res.json(cars);
     })
     .catch(err => {
-        console.log(err);
-        res.json({message: "error!!!"})
+        next(err)
+
     })
 })
 
 
-router.post('/', (req, res) => {
+router.post('/', (req, res, next) => {
     db('cars').insert(req.body)
     .then(ids => {
         db('cars').where({id: ids[0]})
@@ -25,9 +25,36 @@ router.post('/', (req, res) => {
         })
     })
     .catch(err => {
-        console.log(err);
-        res.json({message: "error!!!"})
+        next(err)
     })
+})
+
+router.put('/:id', (req, res, next) => {
+    db('cars').where({id: req.params.id}).update(req.body)
+    .then(numUpdated => {
+        db('cars').where({id: req.params.id})
+        .then(updatedCar =>{
+            res.json(updatedCar)
+        })
+    })
+    .catch(err => {
+        next(err)
+    })
+})
+
+router.delete('/:id', (req, res, next) => {
+    db('cars').where({id: req.params.id}).del()
+    .then(() => {
+        res.status(204).json({message: 'deleted'});
+    })
+    .catch(err => {
+        next(err)
+    })
+})
+
+router.use((err, req, res, next) => {
+    console.log(err);
+    res.json({message: "error!!!"})
 })
 
 module.exports = router;
